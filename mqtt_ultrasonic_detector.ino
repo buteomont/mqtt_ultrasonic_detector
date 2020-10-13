fc2b3af812849f87d4e35d8c87b33c63b225f918
buteomont@gmail.com <buteomont@gmail.com>
2020-10-13 06:58:22 -0500

    Code compiles but not yet tested

--- mqtt_ultrasonic_detector.ino
+++ mqtt_ultrasonic_detector.ino
@@ -22,9 +22,6 @@
 #include <EEPROM.h>
 #include "mqtt_ultrasonic_detector.h"
 
-//PubSubClient callback function header.  This must appear before the PubSubClient constructor.
-void incomingMqttHandler(char* reqTopic, byte* payload, unsigned int length);
-
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
@@ -40,6 +37,8 @@ typedef struct
   char mqttUsername[USERNAME_SIZE]="";
   char mqttPassword[PASSWORD_SIZE]="";
   char mqttTopicRoot[MQTT_TOPIC_SIZE]="";
+  int minimumPresenceDistance=0;  // Item is present if distance is greater than this
+  int maximumPresenceDistance=400;// and distance is less than this
   } conf;
 
 conf settings; //all settings in one struct makes it easier to store in EEPROM
@@ -48,6 +47,8 @@ boolean settingsAreValid=false;
 String commandString = "";     // a String to hold incoming commands from serial
 bool commandComplete = false;  // goes true when enter is pressed
 
+int distance=0; //the last measurement
+boolean itemPresent=false; //TRUE if some object is within range window
 
 void setup() 
   {
@@ -100,13 +101,10 @@ void setup()
 
     // ********************* Initialize the MQTT connection
     mqttClient.setServer(settings.mqttBrokerAddress, settings.mqttBrokerPort);
-    mqttClient.setCallback(incomingMqttHandler);
     
 //    delay(2000);  //give wifi a chance to warm up
     reconnect();  // connect to the MQTT broker   
     }
-    
-  Serial.println("\nConfiguration is done via serial connection.  You can enter:\n");
   
   
   }
@@ -114,7 +112,9 @@ void setup()
  
 void loop()
   {
-  int distance=measure();
+  distance=measure();
+  itemPresent=distance>settings.minimumPresenceDistance 
+              && distance<settings.maximumPresenceDistance;
   Serial.print("Nearest object in centimeters: ");
   Serial.println(distance);
   delay(5000);    
@@ -183,4 +183,324 @@ int getDistance()
   
   //Calculate the distance (in cm) based on the speed of sound.  This is nominally at sea level
   return duration/58.2;
+  }
+
+
+void showSettings()
+  {
+  Serial.print("broker=<MQTT broker host name or address> (");
+  Serial.print(settings.mqttBrokerAddress);
+  Serial.println(")");
+  Serial.print("port=<port number>   (");
+  Serial.print(settings.mqttBrokerPort);
+  Serial.println(")");
+  Serial.print("topicRoot=<topic root> (");
+  Serial.print(settings.mqttTopicRoot);
+  Serial.println(")  Note: must end with \"/\"");  
+  Serial.print("user=<mqtt user> (");
+  Serial.print(settings.mqttUsername);
+  Serial.println(")");
+  Serial.print("pass=<mqtt password> (");
+  Serial.print(settings.mqttPassword);
+  Serial.println(")");
+  Serial.print("ssid=<wifi ssid> (");
+  Serial.print(settings.ssid);
+  Serial.println(")");
+  Serial.print("wifipass=<wifi password> (");
+  Serial.print(settings.wifiPassword);
+  Serial.println(")");
+  Serial.print("mindistance=<minimum presence distance> (");
+  Serial.print(settings.minimumPresenceDistance);
+  Serial.println(")");
+  Serial.print("maxdistance=<maximum presence distance> (");
+  Serial.print(settings.maximumPresenceDistance);
+  Serial.println(")");
+  Serial.println("\"reboot=yes\" to reboot the controller");
+  Serial.println("\n*** Use \"factorydefaults=yes\" to reset all settings ***\n");
+  }
+
+/*
+ * Reconnect to the MQTT broker
+ */
+void reconnect() 
+  {
+  // Create a random client ID
+  String clientId = "ESP8266Client-";
+  clientId += String(random(0xffff), HEX);
+  
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) 
+    {
+    Serial.print("Attempting MQTT connection...");
+    
+    // Attempt to connect
+    if (mqttClient.connect(clientId.c_str(),settings.mqttUsername,settings.mqttPassword))
+      {
+      Serial.println("connected to MQTT broker.");
+
+      //resubscribe to the incoming message topic
+      char topic[MQTT_TOPIC_SIZE];
+      strcpy(topic,settings.mqttTopicRoot);
+      strcat(topic,MQTT_TOPIC_COMMAND_REQUEST);
+      mqttClient.subscribe(topic);
+      }
+    else 
+      {
+      Serial.print("failed, rc=");
+      Serial.println(mqttClient.state());
+      Serial.println("Will try again in 5 seconds");
+      
+      // Wait a second before retrying
+      // In the meantime check for input in case something needs to be changed to make it work
+      checkForCommand(); 
+      
+      delay(1000);
+      }
+    }
+  }
+
+/*
+ * Check for configuration input via the serial port.  Return a null string 
+ * if no input is available or return the complete line otherwise.
+ */
+String getConfigCommand()
+  {
+  if (commandComplete) 
+    {
+    Serial.println(commandString);
+    String newCommand=commandString;
+
+    commandString = "";
+    commandComplete = false;
+    return newCommand;
+    }
+  else return "";
+  }
+
+void processCommand(String cmd)
+  {
+  const char *str=cmd.c_str();
+  char *val=NULL;
+  char *nme=strtok((char *)str,"=");
+  if (nme!=NULL)
+    val=strtok(NULL,"=");
+
+  //Get rid of the carriage return
+  if (val!=NULL && strlen(val)>0 && val[strlen(val)-1]==13)
+    val[strlen(val)-1]=0; 
+
+  if (nme==NULL || val==NULL || strlen(nme)==0 || strlen(val)==0)
+    {
+    showSettings();
+    
+    return;  
+    }
+  else if (strcmp(nme,"broker")==0)
+    {
+    strcpy(settings.mqttBrokerAddress,val);
+    saveSettings();
+    if (settingsAreValid)
+      ESP.restart();
+    }
+  else if (strcmp(nme,"port")==0)
+    {
+    settings.mqttBrokerPort=atoi(val);
+    saveSettings();
+    if (settingsAreValid)
+      ESP.restart();
+    }
+  else if (strcmp(nme,"topicRoot")==0)
+    {
+    strcpy(settings.mqttTopicRoot,val);
+    saveSettings();
+    if (settingsAreValid)
+      ESP.restart();
+    }
+  else if (strcmp(nme,"user")==0)
+    {
+    strcpy(settings.mqttUsername,val);
+    saveSettings();
+    if (settingsAreValid)
+      ESP.restart();
+    }
+  else if (strcmp(nme,"pass")==0)
+    {
+    strcpy(settings.mqttPassword,val);
+    saveSettings();
+    if (settingsAreValid)
+      ESP.restart();
+    }
+  else if (strcmp(nme,"ssid")==0)
+    {
+    strcpy(settings.ssid,val);
+    saveSettings();
+    if (settingsAreValid)
+      ESP.restart();
+    }
+  else if (strcmp(nme,"wifipass")==0)
+    {
+    strcpy(settings.wifiPassword,val);
+    saveSettings();
+    if (settingsAreValid)
+      ESP.restart();
+    }
+  else if (strcmp(nme,"mindistance")==0)
+    {
+    settings.minimumPresenceDistance=atoi(val);
+    saveSettings();
+    }
+  else if (strcmp(nme,"maxdistance")==0)
+    {
+    settings.maximumPresenceDistance=atoi(val);
+    saveSettings();
+    }
+  else if ((strcmp(nme,"factorydefaults")==0) && (strcmp(val,"yes")==0)) //reset all eeprom settings
+    {
+    Serial.println("\n*********************** Resetting EEPROM Values ************************");
+    initializeSettings();
+    saveSettings();
+    delay(2000);
+    ESP.restart();
+    }
+  else
+    showSettings();
+  return;
+  }
+
+void initializeSettings()
+  {
+  settings.validConfig=0; 
+  strcpy(settings.ssid,"");
+  strcpy(settings.wifiPassword,"");
+  strcpy(settings.mqttBrokerAddress,""); //default
+  settings.mqttBrokerPort=1883;
+  strcpy(settings.mqttUsername,"");
+  strcpy(settings.mqttPassword,"");
+  strcpy(settings.mqttTopicRoot,"");
+  settings.minimumPresenceDistance=0;
+  settings.maximumPresenceDistance=400;
+  }
+
+void checkForCommand()
+  {
+  if (Serial.available())
+    {
+    serialEvent();
+    String cmd=getConfigCommand();
+    if (cmd.length()>0)
+      {
+      processCommand(cmd);
+      }
+    }
+  }
+
+
+/************************
+ * Do the MQTT thing
+ ************************/
+void report()
+  {  
+  char topic[MQTT_TOPIC_SIZE];
+  char reading[18];
+  boolean success=false;
+
+  //publish the distance measurement
+  strcpy(topic,settings.mqttTopicRoot);
+  strcat(topic,MQTT_TOPIC_DISTANCE);
+  sprintf(reading,"%d",distance); 
+  success=publish(topic,reading);
+  if (!success)
+    Serial.println("************ Failed publishing distance measurement!");
+
+  //publish the object detection state
+  strcpy(topic,settings.mqttTopicRoot);
+  strcat(topic,MQTT_TOPIC_STATE);
+  sprintf(reading,"%s",itemPresent?"YES":"NO"); //item within range window
+  success=publish(topic,reading);
+  if (!success)
+    Serial.println("************ Failed publishing sensor state!");
+
+  }
+
+boolean publish(char* topic, char* reading)
+  {
+  Serial.print(topic);
+  Serial.print(" ");
+  Serial.println(reading);
+  return mqttClient.publish(topic,reading);
+  }
+
+  
+/*
+*  Initialize the settings from eeprom and determine if they are valid
+*/
+void loadSettings()
+  {
+  EEPROM.get(0,settings);
+  if (settings.validConfig==VALID_SETTINGS_FLAG)    //skip loading stuff if it's never been written
+    {
+    settingsAreValid=true;
+    Serial.println("Loaded configuration values from EEPROM");
+//    showSettings();
+    }
+  else
+    {
+    Serial.println("Skipping load from EEPROM, device not configured.");    
+    settingsAreValid=false;
+    }
+  }
+
+/*
+ * Save the settings to EEPROM. Set the valid flag if everything is filled in.
+ */
+boolean saveSettings()
+  {
+  if (strlen(settings.ssid)>0 &&
+    strlen(settings.wifiPassword)>0 &&
+    strlen(settings.mqttBrokerAddress)>0 &&
+    settings.mqttBrokerPort!=0 &&
+//    strlen(settings.mqttUsername)>0 &&
+//    strlen(settings.mqttPassword)>0 &&
+    strlen(settings.mqttTopicRoot)>0)
+    {
+    Serial.println("Settings deemed complete");
+    settings.validConfig=VALID_SETTINGS_FLAG;
+    settingsAreValid=true;
+    }
+  else
+    {
+    Serial.println("Settings still incomplete");
+    settings.validConfig=0;
+    settingsAreValid=false;
+    }
+    
+  EEPROM.put(0,settings);
+  return EEPROM.commit();
+  }
+
+  
+/*
+  SerialEvent occurs whenever a new data comes in the hardware serial RX. This
+  routine is run between each time loop() runs, so using delay inside loop can
+  delay response. Multiple bytes of data may be available.
+*/
+void serialEvent() 
+  {
+  while (Serial.available()) 
+    {
+    // get the new byte
+    char inChar = (char)Serial.read();
+
+    // if the incoming character is a newline, set a flag so the main loop can
+    // do something about it 
+    if (inChar == '\n') 
+      {
+      commandComplete = true;
+      }
+    else
+      {
+      // add it to the inputString 
+      commandString += inChar;
+      }
+    }
   }
