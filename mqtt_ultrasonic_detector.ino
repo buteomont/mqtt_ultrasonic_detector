9b5da9cebe803dadfada1a981c562bc96ae0ffa2
buteomont@gmail.com <buteomont@gmail.com>
2020-10-13 06:14:39 -0500

    development commit to get started

--- mqtt_ultrasonic_detector.ino
+++ mqtt_ultrasonic_detector.ino
@@ -17,7 +17,37 @@
  */
 #define VERSION "20.10.13.1"  //remember to update this after every change! YY.MM.DD.REV
  
+#include <PubSubClient.h> 
+#include <ESP8266WiFi.h>
+#include <EEPROM.h>
 #include "mqtt_ultrasonic_detector.h"
+
+//PubSubClient callback function header.  This must appear before the PubSubClient constructor.
+void incomingMqttHandler(char* reqTopic, byte* payload, unsigned int length);
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+// These are the settings that get stored in EEPROM.  They are all in one struct which
+// makes it easier to store and retrieve from EEPROM.
+typedef struct 
+  {
+  unsigned int validConfig=0; 
+  char ssid[SSID_SIZE] = "";
+  char wifiPassword[PASSWORD_SIZE] = "";
+  char mqttBrokerAddress[ADDRESS_SIZE]=""; //default
+  int mqttBrokerPort=1883;
+  char mqttUsername[USERNAME_SIZE]="";
+  char mqttPassword[PASSWORD_SIZE]="";
+  char mqttTopicRoot[MQTT_TOPIC_SIZE]="";
+  } conf;
+
+conf settings; //all settings in one struct makes it easier to store in EEPROM
+boolean settingsAreValid=false;
+
+String commandString = "";     // a String to hold incoming commands from serial
+bool commandComplete = false;  // goes true when enter is pressed
+
 
 void setup() 
   {
@@ -31,6 +61,53 @@ void setup()
   Serial.println();
   
   while (!Serial); // wait here for serial port to connect.
+
+  EEPROM.begin(sizeof(settings)); //fire up the eeprom section of flash
+  Serial.print("Settings object size=");
+  Serial.println(sizeof(settings));
+    
+  commandString.reserve(200); // reserve 200 bytes of serial buffer space for incoming command string
+
+  loadSettings(); //set the values from eeprom
+  if (settings.mqttBrokerPort < 0) //then this must be the first powerup
+    {
+    Serial.println("\n*********************** Resetting All EEPROM Values ************************");
+    initializeSettings();
+    saveSettings();
+    delay(2000);
+    ESP.restart();
+    }
+
+  if (settingsAreValid)
+    {
+    // ********************* attempt to connect to Wifi network
+    Serial.print("Attempting to connect to WPA SSID \"");
+    Serial.print(settings.ssid);
+    Serial.println("\"");
+    
+    WiFi.mode(WIFI_STA); //station mode, we are only a client in the wifi world
+    WiFi.begin(settings.ssid, settings.wifiPassword);
+    while (WiFi.status() != WL_CONNECTED) 
+      {
+      // not yet connected
+      Serial.print(".");
+      checkForCommand(); // Check for input in case something needs to be changed to work
+      delay(500);
+      }
+  
+    Serial.println("Connected to network.");
+    Serial.println();
+
+    // ********************* Initialize the MQTT connection
+    mqttClient.setServer(settings.mqttBrokerAddress, settings.mqttBrokerPort);
+    mqttClient.setCallback(incomingMqttHandler);
+    
+//    delay(2000);  //give wifi a chance to warm up
+    reconnect();  // connect to the MQTT broker   
+    }
+    
+  Serial.println("\nConfiguration is done via serial connection.  You can enter:\n");
+  
   
   }
 
